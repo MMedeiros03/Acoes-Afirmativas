7b16e26857c2b1687bc75475796e71a046e22436
MMedeiros03 <matheusmedeiros2003@gmail.com>
2021-10-04 22:44:18 -0300

    criando o app projetos e seus atributos

---
+++ projetos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Projetos(models.Model):
+    nome          = models.CharField(max_length=250,blank=False,null=False)
+    tematica      = models.CharField(max_length=250,blank=False,null=False)
+    objetivo      = models.CharField(max_length=500,blank=False,null=False)
+    justificativa = models.TextField(max_length=2000,blank=False,null=False)
+    publico_alvo  = models.CharField(max_length=250,blank=False,null=False)
+    data_criacao  = models.DateTimeField(auto_now=True)
+    data_atualizacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
+    
