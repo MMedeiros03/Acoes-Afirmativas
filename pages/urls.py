4c6a6d5cc4e4fa735139b5fd5c2d06845c76af77
Medeiross03 <matheusmedeiros2003@gmail.com>
2021-11-30 21:31:27 -0300

    Merge branch 'main' of https://github.com/MMedeiros03/Acoes-Afirmativas

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from projetos.views import Cadastrar_Projeto
-from django.views.generic import RedirectView
+from projetos.views import Cadastrar_Projeto,
+from . import views
 
 app_name = "pages"
 
 urlpatterns = [
-    path('',RedirectView.as_view(url="/home/")),
+    path("", views.HomePageView.as_view(), name="home"),
     path("projeto/cadastrar",Cadastrar_Projeto),
 ]
